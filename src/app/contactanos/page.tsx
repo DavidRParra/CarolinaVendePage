00d8d27efbb141a3243e2b7655cb23d8d14a142c
David R. Parra <davidparra9727@gmail.com>
2025-09-25 08:53:33 -0400

    Cambio al formulario (Se le agregaron los campos faltantes al formulario)

--- src/app/contactanos/page.tsx
+++ src/app/contactanos/page.tsx
@@ -13,38 +13,64 @@ function CitasPage() {
                 </p>
 
                 
-                <form action="" className="grid grid-cols-2 gap-6 w-[70%] border font-bold text-[#75512f] border-[#75512f] p-5 rounded-lg">
+                <form action="" className=" gap-6 w-[70%] border font-bold text-[#75512f] border-[#75512f] p-5 rounded-lg">
 
-                    <div className="flex flex-col">
-                        <label htmlFor="nombre">Nombre*</label>
-                        <input required type="text" placeholder="María" className="rounded-full border px-4 text-black"/>
-                    </div>
+                    <div className="grid grid-cols-2 gap-6">
 
-                    <div className="flex flex-col">
-                        <label htmlFor="nombre">Apellido*</label>
-                        <input required type="text" placeholder="Rosario" className="rounded-full border px-4 text-black"/>
-                    </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Nombre*</label>
+                            <input required type="text" placeholder="María" className="rounded-full border px-4 text-black"/>
+                        </div>
 
-                    <div className="flex flex-col">
-                        <label htmlFor="nombre">Telefono*</label>
-                        <input required type="number" placeholder="8096760000" className="rounded-full border px-4 text-black"/>
-                    </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Apellido*</label>
+                            <input required type="text" placeholder="Rosario" className="rounded-full border px-4 text-black"/>
+                        </div>
 
-                    <div className="flex flex-col">
-                        <label htmlFor="nombre">Email*</label>
-                        <input required type="text" placeholder="ejemplo@ejemplo.com" className="rounded-full border px-4 text-black"/>
-                    </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Telefono*</label>
+                            <input required type="number" placeholder="8096760000" className="rounded-full border px-4 text-black"/>
+                        </div>
 
-                    <div className="flex flex-col">
-                        <label htmlFor="nombre">Fecha de la cita*</label>
-                        <input required type="datetime-local" className="rounded-full border px-4 text-black"/>
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Email*</label>
+                            <input required type="text" placeholder="ejemplo@ejemplo.com" className="rounded-full border px-4 text-black"/>
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Fecha de la cita*</label>
+
+                            <select name="tipo-consulta" id="tipo-consulta" className="flex border rounded-full py-1 px-6 text-black" required>
+                                <option value="default" hidden>--Sleccione una opcion--</option>
+                                <option value="compra">Compra</option>
+                                <option value="alquiler">Alquiler</option>
+                                <option value="venta">Venta</option>
+                                <option value="evaluacion">Evaluación</option>
+                                <option value="hipoteca">Hipoteca</option>
+                                <option value="compra">Otro</option>
+                            </select>
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="nombre">Fecha de la cita*</label>
+                            <input required type="date" className="rounded-full border px-4 text-black"/>
+                        </div>
+
+                        <div className="flex flex-col my-10 w-full col-span-2">
+                            <label htmlFor="" className="text-[#75512f]">
+                                Mensaje
+                                <span className="text-red-700">*</span>
+                            </label>
+                            <textarea name="" id="" rows={5} className="flex border rounded-lg py-2 px-4 resize-none"></textarea>
+                        </div>
+
                     </div>
 
                     <div className="flex items-end text-white">
                         <button 
                             className="
-                                flex items-center justify-between 
-                                w-full 
+                                flex items-center justify-center gap-4 
+                                mx-auto
                                 border border-transparent rounded-full 
                                 bg-[#75512f] py-1 px-4 cursor-pointer 
                                 hover:text-[#75512f] hover:border-[#75512f] hover:bg-white"
@@ -55,6 +81,7 @@ function CitasPage() {
                     </div>
 
                 </form>
+                
 
 
 
